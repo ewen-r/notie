fb454ce47e7bc4db596546616eb89af986e57e75
ewen-r <ewen.reynolds@gmail.com>
2023-08-07 12:29:20 +0100

    Add App.jsx

---
+++ src/components/App.jsx
@@ -0,0 +1,18 @@
+import React from "react";
+import Header from "./Header";
+import Footer from "./Footer";
+import Note from "./Note";
+
+/** Main App. */
+function App() {
+  return (
+    <div>
+      <Header />
+      <Note />
+      <Footer />
+    </div>
+  );
+}
+
+// Export the app
+export default App;
