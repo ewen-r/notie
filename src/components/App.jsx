2b859b9048ac5c141f398c1df9592e76b2b39985
ewen-r <ewen.reynolds@gmail.com>
2023-08-07 15:09:23 +0100

    Added some dummy note data.

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -2,13 +2,28 @@ import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
 import Note from "./Note";
+import { dummyNotes } from "./devComponents/dummyNotes";
+
+
+/** Creates a Note element from input note object.
+  * @param {any} note Input note object.
+ */
+function createNote(note) {
+  return (
+    <Note
+      title={note.title}
+      content={note.content}
+    />
+  );
+}
+
 
 /** Main App. */
 function App() {
   return (
     <div>
       <Header />
-      <Note />
+      { dummyNotes.map(n => createNote(n)) }
       <Footer />
     </div>
   );
--- src/components/Note.jsx
+++ src/components/Note.jsx
@@ -1,14 +1,14 @@
 import React from "react";
 
-/** Component to display Note. */
-function Note() {
-  const title = 'Note title here';
-  const content = 'Note content here.';
+/** Component to display Note.
+  * @param {any} props Input properties.
+*/
+function Note(props) {
 
   return (
     <div className="note">
-      <h1>{title}</h1>
-      <p>{content}</p>
+      <h1>{props.title || 'Untitled note'}</h1>
+      <p>{props.content || ''}</p>
     </div>
   );
 }
